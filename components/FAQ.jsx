6c22d3d411e89e7fd3f61c365d34b0ae97faa6bf
Phanuelle <phanuellienou@gmail.com>
2025-02-12 13:15:20 -0500

    creation du composant header et FAQ

---
+++ components/FAQ.jsx
@@ -0,0 +1,31 @@
+"use client";
+import { useState } from "react";
+import { FaAngleDown } from "react-icons/fa6";
+import { IoPlay } from "react-icons/io5";
+import styles from "@/components/FAQ.module.css";
+
+export default function FAQ({ question, response }) {
+ const [isResponse, setResponse] = useState(false);
+
+ const toggleResponse = () => {
+  setResponse(!isResponse);
+ };
+
+ return (
+  <div className={styles.faqContainer}>
+   {/* Question */}
+   <div className={styles.faqHead} onClick={toggleResponse}>
+    <IoPlay className={styles.iconPlay} />
+    <p className={styles.faqQuestion}>{question}</p>
+    <FaAngleDown className={styles.iconArrow} />
+   </div>
+
+   {/* Réponse */}
+   {isResponse && (
+    <div className={styles.faqResponse}>
+     <p>{response}</p>
+    </div>
+   )}
+  </div>
+ );
+}
