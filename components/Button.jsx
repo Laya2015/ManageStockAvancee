84c31d164e8db18c0a49a8e6787e795d267a1081
Ousameur Elias <ousameurelias@gmail.com>
2025-02-13 14:48:26 -0500

    ajouter le type pour le button

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,9 +1,10 @@
 import styles from "./Button.module.css";
 
-export default function Button({ texte, className, active }) {
+export default function Button({ texte, className, active, type}) {
  return (
   <button
-   className={`${styles.button} ${className} ${active ? styles.active : ""}`}>
+   className={`${styles.button} ${className} ${active ? styles.active : ""}`}
+   type={type}>
    {texte}
   </button>
  );
