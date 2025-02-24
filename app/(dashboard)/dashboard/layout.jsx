51e423e0c306a3529badb2d49058087b2c763fa1
Ousameur Elias <ousameurelias@gmail.com>
2025-02-24 10:58:41 -0500

    ajouter des logos pour sideBar dans le dossier public et creation de sideBar

--- app/(dashboard)/dashboard/layout.jsx
+++ app/(dashboard)/dashboard/layout.jsx
@@ -1,5 +1,16 @@
 import style from "./layout.module.css";
 import "@/app/globals.css";
+import Image from "next/image";
+import Link from "next/link";
+import userLogo from "@/public/user.svg";
+import inventoryLogo from "@/public/intentory.svg";
+import deconexionLogo from "@/public/log-out.svg";
+import receptionLogo from "@/public/reception.svg";
+import retourLogo from "@/public/retour.svg";
+import rechercheLogo from "@/public/search.svg";
+import accueilLogo from "@/public/home.svg";
+
+import logo from "@/public/Logo-White.png";
 
 import { Inter } from "next/font/google";
 
@@ -20,8 +31,62 @@ export default function DashboardLayout({ children }) {
   return (
     <html lang="fr" className={`${inter.variable}`}>
       <body className={style.body}>
-        <div><h3>dashboard Layout</h3></div>
+        <div className={style.bar}>
+          <Image src={logo} alt="logo de ManageStock" />
+        </div>
         <main className={style.main}>{children}</main>
+        <div className={style.sideBar}>
+          <nav>
+            <ul>
+              <li>
+                <div className={style.selection}>
+                  <span className={style.circleH}></span>
+                  <span className={style.circleB}></span>
+                  <Image src={accueilLogo} alt="logo Accueil" />
+                  <Link href="#">Accueil</Link>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <Image src={rechercheLogo} alt="logo recherche" />
+                  <Link href="#">Recherche Article</Link>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <Image src={receptionLogo} alt="logo reception" />
+                  <Link href="#">Gestions des Réceptions</Link>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <Image src={retourLogo} alt="logo retour" />
+                  <Link href="#">Traitement des Retours</Link>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <Image src={inventoryLogo} alt="logo inventory" />
+                  <Link href="#">Article & Inventaire</Link>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <Image src={userLogo} alt="logo Utilisateur" />
+                  <Link href="#">Gestion des Utilisateurs</Link>
+                </div>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <div>
+                  <Image src={deconexionLogo} alt="logo deconixion" />
+                  <Link href="#">Déconnexion</Link>
+                </div>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </body>
     </html>
   );
--- app/(dashboard)/dashboard/page.jsx
+++ app/(dashboard)/dashboard/page.jsx
@@ -1,8 +1,7 @@
-export default function Dashboard(){
-    return(
-        <>
-        <h1>dashboard page</h1>
-        </>
-    )
+export default function Dashboard() {
+  return (
+    <>
+      <h1>dashboard page</h1>
+    </>
+  );
 }
-
