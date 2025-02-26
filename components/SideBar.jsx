3dd6d028b5c7329f28fc8a2a2ff772c0efa04a0c
Ousameur Elias <ousameurelias@gmail.com>
2025-02-26 12:02:26 -0500

    modifier les logos de sideBar

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -3,18 +3,28 @@ import style from "./Sidebar.module.css";
 import Image from "next/image";
 
 import Link from "next/link";
-import userLogo from "@/public/user.svg";
-import inventoryLogo from "@/public/intentory.svg";
-import deconexionLogo from "@/public/log-out.svg";
-import receptionLogo from "@/public/reception.svg";
-import retourLogo from "@/public/retour.svg";
-import rechercheLogo from "@/public/search.svg";
-import accueilLogo from "@/public/home.svg";
 import { usePathname } from "next/navigation";
 
+import { IoHomeOutline, IoSearch } from "react-icons/io5";
+import { BsBoxSeam } from "react-icons/bs";
+import { GiBoxUnpacking } from "react-icons/gi";
+import { MdOutlineInventory2, MdOutlineLogout } from "react-icons/md";
+import { FiUsers } from "react-icons/fi";
+
+/**
+ * Composant SideBar
+ *
+ * Ce composant affiche une barre latérale de navigation pour le tableau de bord.
+ * Il met en surbrillance l'élément actif en fonction de l'URL actuelle.
+ *
+ * Utilise `usePathname()` pour récupérer l'URL et applique une classe spécifique
+ * aux liens actifs.
+ */
 export default function SideBar() {
+  // Récupération du chemin actuel pour déterminer l'élément actif
   const url = usePathname();
   console.log("pathName : " + url);
+
   return (
     <div className={style.sideBar}>
       <nav>
@@ -24,12 +34,15 @@ export default function SideBar() {
               <div className={style.selection}>
                 <span className={style.circleH}></span>
                 <span className={style.circleB}></span>
-                <Image src={accueilLogo} alt="logo Accueil" />
+                <IoHomeOutline
+                  className={style.sideBarLogo}
+                  style={{ color: "black" }}
+                />
                 <Link href="/dashboard">Accueil</Link>
               </div>
             ) : (
               <div>
-                <Image src={accueilLogo} alt="logo Accueil" />
+                <IoHomeOutline className={style.sideBarLogo} />
                 <Link href="/dashboard">Accueil</Link>
               </div>
             )}
@@ -39,12 +52,15 @@ export default function SideBar() {
               <div className={style.selection}>
                 <span className={style.circleH}></span>
                 <span className={style.circleB}></span>
-                <Image src={rechercheLogo} alt="logo recherche" />
+                <IoSearch
+                  className={style.sideBarLogo}
+                  style={{ color: "black" }}
+                />
                 <Link href="/dashboard/recherche">Recherche Article</Link>
               </div>
             ) : (
               <div>
-                <Image src={rechercheLogo} alt="logo recherche" />
+                <IoSearch className={style.sideBarLogo} />
                 <Link href="/dashboard/recherche">Recherche Article</Link>
               </div>
             )}
@@ -54,14 +70,17 @@ export default function SideBar() {
               <div className={style.selection}>
                 <span className={style.circleH}></span>
                 <span className={style.circleB}></span>
-                <Image src={receptionLogo} alt="logo reception" />
+                <BsBoxSeam
+                  className={style.sideBarLogo}
+                  style={{ color: "black" }}
+                />
                 <Link href="/dashboard/receptions">
                   Gestions des Réceptions
                 </Link>
               </div>
             ) : (
               <div>
-                <Image src={receptionLogo} alt="logo reception" />
+                <BsBoxSeam className={style.sideBarLogo} />
                 <Link href="/dashboard/receptions">
                   Gestions des Réceptions
                 </Link>
@@ -73,12 +92,15 @@ export default function SideBar() {
               <div className={style.selection}>
                 <span className={style.circleH}></span>
                 <span className={style.circleB}></span>
-                <Image src={retourLogo} alt="logo retour" />
+                <GiBoxUnpacking
+                  className={style.sideBarLogo}
+                  style={{ color: "black" }}
+                />
                 <Link href="/dashboard/retours">Traitement des Retours</Link>
               </div>
             ) : (
               <div>
-                <Image src={retourLogo} alt="logo retour" />
+                <GiBoxUnpacking className={style.sideBarLogo} />
                 <Link href="/dashboard/retours">Traitement des Retours</Link>
               </div>
             )}
@@ -88,12 +110,15 @@ export default function SideBar() {
               <div className={style.selection}>
                 <span className={style.circleH}></span>
                 <span className={style.circleB}></span>
-                <Image src={inventoryLogo} alt="logo inventory" />
+                <MdOutlineInventory2
+                  className={style.sideBarLogo}
+                  style={{ color: "black" }}
+                />
                 <Link href="/dashboard/inventaire">Article & Inventaire</Link>
               </div>
             ) : (
               <div>
-                <Image src={inventoryLogo} alt="logo inventory" />
+                <MdOutlineInventory2 className={style.sideBarLogo} />
                 <Link href="/dashboard/inventaire">Article & Inventaire</Link>
               </div>
             )}
@@ -103,14 +128,17 @@ export default function SideBar() {
               <div className={style.selection}>
                 <span className={style.circleH}></span>
                 <span className={style.circleB}></span>
-                <Image src={userLogo} alt="logo Utilisateur" />
+                <FiUsers
+                  className={style.sideBarLogo}
+                  style={{ color: "black" }}
+                />
                 <Link href="/dashboard/utilisateurs">
                   Gestion des Utilisateurs
                 </Link>
               </div>
             ) : (
               <div>
-                <Image src={userLogo} alt="logo Utilisateur" />
+                <FiUsers className={style.sideBarLogo} />
                 <Link href="/dashboard/utilisateurs">
                   Gestion des Utilisateurs
                 </Link>
@@ -121,7 +149,7 @@ export default function SideBar() {
         <ul>
           <li>
             <div>
-              <Image src={deconexionLogo} alt="logo deconixion" />
+              <MdOutlineLogout className={style.sideBarLogo} />
               <Link href="#">Déconnexion</Link>
             </div>
           </li>
