3a7feb7d0c5a6190f0ca500554a1068c4e2a8281
Ousameur Elias <ousameurelias@gmail.com>
2025-03-01 15:16:17 -0500

    creer la strecture des page login inscription est contactez-nous

---
+++ app/(login)/inscription/page.jsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Inscription() {
+  return (
+    <>
+      <h1>Inscription</h1>
+    </>
+  );
+}
---
+++ app/(login)/login/page.jsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Connexion() {
+  return (
+    <>
+      <h1>Connexion</h1>
+    </>
+  );
+}
