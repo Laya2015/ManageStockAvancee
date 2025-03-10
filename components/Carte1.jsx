7c32749275807d0a32f120191532d5dfe5ac67d0
Anisse <96845859+Laya2015@users.noreply.github.com>
2025-03-09 21:48:53 -0400

    creation de la Carte1.jsx et son css (#14)
    * creation de la Carte1.jsx et son css
    
    * modification apporter au fichier css de la Carte1
    
    ---------
    
    Co-authored-by: Anisse <AnisseBen@users.noreply.github.com>

---
+++ components/Carte1.jsx
@@ -0,0 +1,28 @@
+import styles from "./Carte1.module.css"
+import Image from "next/image"
+
+import Button from "./Button"
+export default function Carte1({ titre, paragraphe, texteButton, activeButton, srcImage1, srcImage2, estVisible, estInverse }) {
+    return <>
+        <section className={`${styles.section} ${estInverse ? styles.inverse : ""}`}>
+            <div className={`${styles.contenu} ${estInverse ? styles.contenuInverse : ""}`}>
+                <h3>{titre}</h3>
+                <p>{paragraphe}</p>
+                <Button texte={texteButton} active={activeButton}></Button>
+            </div>
+            <div className={`${styles.images} ${estInverse ? styles.imagesInverse : ""}`}>
+                {estVisible ?
+                    <div className={`${styles.image1} ${estInverse ? styles.image1Inverse : ""} `}>
+                        <Image src={srcImage1} alt="image" className={styles.renduImage} />
+                    </div>
+                    :
+                    <></>
+                }
+                <div className={`${styles.image2} ${estInverse ? styles.image2Inverse : ""} `}>
+                    <Image src={srcImage2} alt="image" className={styles.renduImage} />
+                </div>
+            </div>
+        </section>
+
+    </>
+}
