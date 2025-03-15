79e7b4d681e5d149a77de2d354ea078a44f25646
Elias Ousameur <ousameurelias@gmail.com>
2025-03-15 00:17:56 -0400

    ajouter onClick ppur le button (#25)

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,11 +1,33 @@
+"use client";
+import { redirect } from "next/navigation";
 import styles from "./Button.module.css";
 
-export default function Button({ texte, className, active, type}) {
- return (
-  <button
-   className={`${styles.button} ${className} ${active ? styles.active : ""}`}
-   type={type}>
-   {texte}
-  </button>
- );
+export default function Button({ texte, className, active, type, goToUrl }) {
+  const goTo = () => {
+    redirect(`${goToUrl}`);
+  };
+  return (
+    <div>
+      {goToUrl ? (
+        <button
+          className={`${styles.button} ${className} ${
+            active ? styles.active : ""
+          }`}
+          type={type}
+          onClick={goTo}
+        >
+          {texte}
+        </button>
+      ) : (
+        <button
+          className={`${styles.button} ${className} ${
+            active ? styles.active : ""
+          }`}
+          type={type}
+        >
+          {texte}
+        </button>
+      )}
+    </div>
+  );
 }
