5a35375c930779d16cc3143c80805f02225c6051
Sreemer1 <159464416+Sreemer1@users.noreply.github.com>
2025-03-17 20:45:09 -0400

    Ajout page retour (#34)
    * Ajout page retour
    
    * ajuster le css des 3 boutons
    
    ---------
    
    Co-authored-by: Ousameur Elias <ousameurelias@gmail.com>

--- app/(dashboard)/dashboard/retours/page.jsx
+++ app/(dashboard)/dashboard/retours/page.jsx
@@ -1,7 +1,79 @@
-export default function Retours(){
-    return(
-        <>
-        <h1>page retour</h1>
-        </>
-    )
+"use client";
+import Button from "@/components/Button";
+import InfoBloc from "@/components/InfoBlock";
+import InputForm from "@/components/InputForm";
+import style from "./page.module.css";
+import datas from "@/data/datas.json";
+import { useState } from "react";
+import styles from "@/components/InputForm.module.css";
+
+export default function retours() {
+  const [isInputVisible, setInputVisible] = useState(false);
+
+  const toggleInputForm = () => {
+    setInputVisible(true);
+  };
+  const handleCloseForm = () => {
+    setInputVisible(false);
+  };
+  const inputFields = [
+    { name: "bonCommande", placeholder: "Bon de commande" },
+    { name: "upeSku", placeholder: "UPE/SKU" },
+    { name: "nomArticle", placeholder: "Nom de l'article" },
+    { name: "fournisseur", placeholder: "Fournisseur" },
+    { name: "quantite", placeholder: "Quantité" },
+  ];
+  return (
+    <>
+      <h1 className={style.titre}>Traitement des retours</h1>
+      {!isInputVisible && (
+        <div className={style.boutons}>
+          <div onClick={toggleInputForm}>
+            <Button
+              className={""}
+              texte={"Nouveau"}
+              active={true}
+              type={"button"}
+            />
+          </div>
+
+          <div onClick={toggleInputForm}>
+            <Button
+              texte={"Modifier"}
+              active={true}
+              className={""}
+              type={"button"}
+            />
+          </div>
+
+          <Button
+            texte={"Supprimer"}
+            active={true}
+            className={""}
+            type={"button"}
+          />
+        </div>
+      )}
+
+      {isInputVisible && (
+        <div className={style.inputform}>
+          <InputForm inputFields={inputFields} onClose={handleCloseForm} />
+        </div>
+      )}
+
+      <div>
+        <InfoBloc
+          defaultTitle={"Bon de Retours"}
+          defaultHeaders={[
+            "Upe/Sku",
+            "Nom d'article",
+            "Fournisseur",
+            "Quantité",
+            "Date",
+          ]}
+          data={datas.retours}
+        />
+      </div>
+    </>
+  );
 }
