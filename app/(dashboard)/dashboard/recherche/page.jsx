6de3d12fa571d7d4d77f6113fd9746920e748304
Ousameur Elias <ousameurelias@gmail.com>
2025-03-18 21:37:11 -0400

    meger la page recherche dans main

--- app/(dashboard)/dashboard/recherche/page.jsx
+++ app/(dashboard)/dashboard/recherche/page.jsx
@@ -1,7 +1,31 @@
-export default function Recherche(){
-    return(
+import InfoBloc from "@/components/InfoBlock.jsx";
+import styles from "./page.module.css";
+import Recherche1 from "@/components/Recherche";
+
+import datas from "@/data/datas.json";
+
+export default function Recherche() {
+    return (
         <>
-        <h1>page rechreche</h1>
+            <div className={styles.contenaire}>
+                <div className={styles.divRecherche}>
+                    <Recherche1 />
+                </div>
+                <div className={styles.infoBloc}>
+                    <InfoBloc
+                        defaultTitle={"Recherche Article"}
+                        defaultHeaders={[
+                            "Upe/Sku",
+                            "Nom d’article",
+                            "Fournisseur",
+                            "Quantité",
+                            "Prix",
+                        ]}
+                        data={datas.produitsRupture}
+                    />
+                </div>
+            </div>
+
         </>
     )
 }
