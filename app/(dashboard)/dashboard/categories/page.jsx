9de59456d0aeebba188ea2b28852038f6219b8e1
Anisse <AnisseBen@users.noreply.github.com>
2025-03-27 21:35:17 -0400

    creation de la page Categorie

---
+++ app/(dashboard)/dashboard/categories/page.jsx
@@ -0,0 +1,15 @@
+"use client"
+import Button from "@/components/Button"
+
+
+
+export default function Categorie() {
+  return <>
+    <div>
+        <Button texte="Nouvelle Categorie" active={true} className={""} type={"button"}/>
+    </div>
+    <div>
+        
+    </div>
+  </>
+}
